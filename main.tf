931b25b9a8eb9876a211fcae244d8dd8c9f5554c
Stéphane Noutsa <stephanenoutsak@gmail.com>
2023-11-24 02:47:51 +0100

    Initial commit

---
+++ main.tf
@@ -0,0 +1,15 @@
+# Autoscaling Group
+resource "aws_autoscaling_group" "asg" {
+  name               = var.asg_name
+  availability_zones = var.availability_zones
+  desired_capacity   = var.desired_capacity
+  max_size           = var.max_size
+  min_size           = var.min_size
+  health_check_type  = var.health_check_type
+  target_group_arns  = var.target_group_arns
+
+  launch_template {
+    id      = var.launch_template_id
+    version = var.launch_template_version
+  }
+}
